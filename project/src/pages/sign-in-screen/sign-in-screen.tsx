3bad843eecbde6f0557fb58943e19e24785b77bc
Stepan <ose.26@mail.ru>
2022-10-21 18:05:50 +0500

    7.15. Обновит компонент SignInScreen. Воспользуемся хуком `useRef` для получения доступа к элементам формы. Нам потребуется взаимодействовать с хранилищем (диспатчить действия), поэтому подключим компонент к нему.

--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,6 +1,34 @@
 import Logo from '../../components/logo/logo';
+import {useNavigate} from 'react-router-dom';
+import {useAppDispatch} from '../../hooks';
+import {FormEvent, useRef} from 'react';
+import {loginAction} from '../../store/api-actions';
+import {AuthData} from '../../types/auth-data';
+import {AppRoute} from '../../const';
 
 function SignInScreen(): JSX.Element {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (emailRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
@@ -10,7 +38,11 @@ function SignInScreen(): JSX.Element {
       </header>
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form
+          action="#"
+          className="sign-in__form"
+          onSubmit={handleSubmit}
+        >
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input
@@ -19,6 +51,7 @@ function SignInScreen(): JSX.Element {
                 placeholder="Email address"
                 name="user-email"
                 id="user-email"
+                ref={emailRef}
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
@@ -29,12 +62,19 @@ function SignInScreen(): JSX.Element {
                 placeholder="Password"
                 name="user-password"
                 id="user-password"
+                ref={passwordRef}
               />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
           <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
+            <button
+              className="sign-in__btn"
+              type="submit"
+              onClick={() => navigate(AppRoute.Root)}
+            >
+              Sign in
+            </button>
           </div>
         </form>
       </div>
